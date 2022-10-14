414bf916abc0b131e3ff889384de38d7ad607e97
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-14 16:23:15 -0700

    added file

--- UC-1-Employee/UC-1-Employee/Program.cs
+++ UC-1-Employee/UC-1-Employee/Program.cs
@@ -2,29 +2,29 @@
 {
     internal class Program
     {
+        public const int IS_PARTTIME = 1;
+        public const int IS_FULLTIME = 2;
+        public const int EMP_RATE_PERHR = 20;
         static void Main(string[] args)
         {
-            int IS_FULLTIME = 2;
-            int IS_PARTTIME = 1;
-            int EMP_RATE_PER_HR = 20;
             int emphrs = 0;
             int empwage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULLTIME)
+            switch (empCheck)
             {
-                emphrs = 8;
+                case 1:
+                    emphrs = 4;
+                    break;
+                case 2:
+                    emphrs = 8;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
             }
-            else if (empCheck == IS_PARTTIME)
-            {
-                emphrs = 4;
-            }
-            else
-            {
-                emphrs = 0;
-            }
-            empwage = emphrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Employee Wage: " + empwage);
+            empwage = emphrs * EMP_RATE_PERHR;
+            Console.WriteLine("Employee Wage " + empwage);
         }
     }
 }
