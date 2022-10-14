05d0e11b1f258e4dafedfa17a731b450eeb09b2d
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-14 16:26:43 -0700

    added file

--- UC-1-Employee/UC-1-Employee/Program.cs
+++ UC-1-Employee/UC-1-Employee/Program.cs
@@ -2,29 +2,36 @@
 {
     internal class Program
     {
-        public const int IS_PARTTIME = 1;
-        public const int IS_FULLTIME = 2;
+        public const int IS_FullTime = 2;
+        public const int IS_PartTime = 1;
         public const int EMP_RATE_PERHR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            int emphrs = 0;
-            int empwage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int emphr = 0;
+            int empWage = 0;
+            int totalempWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case 1:
-                    emphrs = 4;
-                    break;
-                case 2:
-                    emphrs = 8;
-                    break;
-                default:
-                    emphrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        emphr = 4;
+                        break;
+                    case 2:
+                        emphr = 8;
+                        break;
+                    default:
+                        emphr = 0;
+                        break;
+                }
+                empWage = emphr * EMP_RATE_PERHR;
+                totalempWage = totalempWage + empWage;
+                Console.WriteLine("Emp Wage :" + empWage);
             }
-            empwage = emphrs * EMP_RATE_PERHR;
-            Console.WriteLine("Employee Wage " + empwage);
+            Console.WriteLine("Total EmpWage :" + totalempWage);
         }
     }
 }
