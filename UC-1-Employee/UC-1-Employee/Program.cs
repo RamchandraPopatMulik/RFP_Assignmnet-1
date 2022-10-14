9979fb9a925247d47bdb46ea76c8eb8c6035e058
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-14 16:53:25 -0700

    added file

--- UC-1-Employee/UC-1-Employee/Program.cs
+++ UC-1-Employee/UC-1-Employee/Program.cs
@@ -5,16 +5,13 @@
 
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-
-        public static int computeEmpWage()
+ 
+        public static int computeEmpWage(string company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
         {
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -33,16 +30,17 @@
 
                 }
                 totalEmpHrs = totalEmpHrs + empHrs;
-                Console.WriteLine("Total Employee Hrs: " + totalEmpHrs);
+                Console.WriteLine("Day: "+totalWorkingDays + "Emp Hrs : "+ empHrs);
 
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
             Console.WriteLine("Total Employee Wage" + totalEmpWage);
             return totalEmpWage;
         }
         static void Main(string[] args)
         {
-            computeEmpWage();
+            computeEmpWage("TCS",20,2,10);
+            computeEmpWage("TechMahindra", 30, 8, 15);
         }
     }
 }
