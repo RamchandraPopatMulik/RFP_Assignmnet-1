034a715632e3760490d35bf72a3ef443699bd558
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-15 09:51:49 -0700

    Added file

---
+++ UC-1-Employee/UC-1-Employee/EmpWageBuilderObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_1_Employee
+{
+    public class EmpWageBuilderObject
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuilderObject(string company ,int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company; 
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 4;
+                        break;
+                    case 2:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                totalEmpHrs = totalEmpHrs + empHrs;
+                Console.WriteLine("Day: " + totalWorkingDays + "Emp Hrs : " + empHrs);
+
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Employee Wage" + company + "is "+ this.totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for comany : " + this.company + " is: " +this.totalEmpWage;
+        }
+
+    }
+}
--- UC-1-Employee/UC-1-Employee/Program.cs
+++ UC-1-Employee/UC-1-Employee/Program.cs
@@ -1,46 +1,15 @@
 ﻿namespace UC_1_Employee
 {
-    internal class Program
+    public class Program
     {
-
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
- 
-        public static int computeEmpWage(string company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
-        {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case 1:
-                        empHrs = 4;
-                        break;
-                    case 2:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-
-                }
-                totalEmpHrs = totalEmpHrs + empHrs;
-                Console.WriteLine("Day: "+totalWorkingDays + "Emp Hrs : "+ empHrs);
-
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Employee Wage" + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("TCS",20,2,10);
-            computeEmpWage("TechMahindra", 30, 8, 15);
+            EmpWageBuilderObject tcs = new EmpWageBuilderObject("TCS",40,5,15);
+            EmpWageBuilderObject Tech = new EmpWageBuilderObject("TechMahindra",50,4,25);
+            tcs.computeEmpWage();
+            Console.WriteLine(tcs.toString());
+            Tech.computeEmpWage();
+            Console.WriteLine(Tech.toString());
         }
     }
 }
